8365fde63904c60c7e003261538d8a394e3b4fdf
Rebecca VandeWater <becca.vandewater@gmail.com>
2023-04-17 21:27:47 -0600

    Added draw functionality for forms, organization

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
-let contacts = []
+let addElement = document.getElementById("add");
+let deleteElement = document.getElementById("delete");
+
+
+// *SECTION - Draw Forms
+
+// *TODO - Make button disabled instead of adding and deleting 'show'.
+
+
+function drawAddContact(){
+    if(deleteElement.classList.contains("show")){
+        alert("Please press 'Submit' to finish deleting contact, or press 'Cancel'.")
+    } else{
+        addElement.classList.remove("hidden");
+        addElement.classList.add("show");
+    }
+}
+
+function drawDeleteContact(){
+    if(addElement.classList.contains("show")){
+        alert("Please press 'Submit' to finish adding contact, or press 'Cancel'.")
+    } else{
+        deleteElement.classList.remove("hidden");
+        deleteElement.classList.add("show");
+    }
+}
+
+function drawContactList(){
+
+}
+
+
+// *SECTION - Add Contact
+
+function addContact(event){
+    event.preventDefault();
+
+
+    addElement.classList.add("hidden")
+}
+
+function cancelAdd(event){
+    event.preventDefault();
+
+    addElement.classList.add("hidden");
+    addElement.classList.remove("show");
+}
+
+
+// *SECTION - Delete Contact
+
+function deleteContact(event){
+    event.preventDefault();
+    
+    deleteElement.classList.add("hidden");
+}
+
+function cancelDelete(event){
+    event.preventDefault();
+
+    deleteElement.classList.add("hidden");
+    deleteElement.classList.remove("show");
+}
